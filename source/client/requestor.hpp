8f83b0e530f5c7f629659b25360431ad0c9563bb
半介莽夫 <2634716776@qq.com>
2025-04-25 18:37:22 +0800

    update: rpc_router done, next requestor

--- source/client/requestor.hpp
+++ source/client/requestor.hpp
@@ -10,172 +10,190 @@
 
 #include <unordered_map>
 
-namespace Rpc
-{
-    namespace Client
+namespace Rpc {
+namespace Client {
+class Requestor {
+  // 面向用户提供
+  // 主要提供send发送接口( 需要提供两个重载 回调发送和异步发送 )
+  // 其次是提供一个对服务端响应的一个onResponse
+  // 通过onResponse将消息传送给Dispatcher进行请求分发从而进行下一步的处理
+public:
+  using ptr = std::shared_ptr<Requestor>;
+
+  /*
+   * 下面一个Callback与一个Async的future分别表示:
+   * 1. RequestCallback: 用于回调请求
+   * 2. AsyncResponse: 用于异步请求
+   */
+  using RequestCallback = std::function<void(
+      const BaseMessage::ptr &)>; // 用function包装器包装一个函数类型
+
+  using AsyncResponse = std::future<BaseMessage::ptr>; // 对future进行封装
+
+  struct RequestDescribe // 内部类 用来描述请求信息
+  {
+    using ptr = std::shared_ptr<RequestDescribe>; // 指针描述
+    RType rtype; // 请求类型 请求类型有两种, 分别为异步请求类型与回调请求类型
+    BaseMessage::ptr request; // 请求消息
+    std::promise<BaseMessage::ptr>
+        response; // 通过get_future()可以获取一个与该promise相关联的future对象
+    RequestCallback callback; // 消息类型
+  }; // struct RequestDescribe
+
+  void
+  onResponse(const BaseConnection::ptr &,
+             BaseMessage::ptr &); // 注册进Dispatcher中的对响应进行的业务处理
+
+  /* 为用户提供三个send接口 通过send接口向服务端发送数据*/
+  bool send(const BaseConnection::ptr &, const BaseMessage::ptr &,
+            const RequestCallback &); // 回调请求
+
+  bool send(const BaseConnection::ptr &, const BaseMessage::ptr &,
+            AsyncResponse &); // 异步请求
+
+  bool send(const BaseConnection::ptr &, const BaseMessage::ptr &,
+            BaseMessage::ptr &); // 同步请求
+
+private:
+  /*
+   * 在 Requestor 类中存在一个用来进行映射的哈希表
+   * 需要为用户提供管理该哈希表的接口
+   *
+   * 对临界资源进行访问都需要保证线程安全
+   */
+  RequestDescribe::ptr
+  newDestribe(const BaseMessage::ptr &, RType,
+              const RequestCallback &cb = nullptr); // 创建一个请求描述
+
+  RequestDescribe::ptr
+  getDescribe(const std::string &); // 通过rid找到对应的请求描述
+
+  void removeDescribe(const std::string &); // 通过rid删除对应的请求描述
+private:
+  std::mutex _mutex; // 保护_request_describe 临界资源
+  std::unordered_map<std::string, RequestDescribe::ptr>
+      _request_describes; // rid 与 请求描述的映射关系
+
+}; // class Requestor
+
+void Requestor::onResponse(const BaseConnection::ptr &con,
+                           BaseMessage::ptr &res) {
+  /*
+   通过rid找到对应的请求描述
+   若未找到则表示该响应不存在对应的请求描述
+   若找到则判断该响应对应的请求描述的rtype
+   若rtype为RType::SYNC 则调用callback
+   若rtype为RType::ASYNC 则调用promise的set_value
+   处理完毕后删除对应的请求描述 以免内存泄漏
+  */
+  auto describe = getDescribe(res->rid());
+  if (describe.get() == nullptr) {
+    ELOG("not found request describe for response rid:{ %s }\n",
+         res->rid().c_str());
+    return; // 未找到对应的请求描述 无需处理
+  }
+  if (describe->rtype == RType::REQ_CALLBACK) {
+    if (describe->callback) // 若callback存在 则调用callback
     {
-        class Requestor
-        { // 面向用户提供
-          // 主要提供send发送接口( 需要提供两个重载 回调发送和异步发送 )
-          // 其次是提供一个对服务端响应的一个onResponse 通过 onResponse将消息传送给Dispatcher进行请求分发从而进行下一步的处理
-        public:
-            using ptr = std::shared_ptr<Requestor>;
+      describe->callback(res);    // 同步请求 调用callback
+      removeDescribe(res->rid()); // 删除对应的请求描述
+    } else {
+      ELOG("callback is empty"); // 若callback为空 则不调用callback
+    }
+  } else if (describe->rtype == RType::REQ_ASYNC) {
+    describe->response.set_value(res); // 异步请求 调用promise的set_value
+    removeDescribe(res->rid());        // 删除对应的请求描述
+  } else {
+    ELOG("unknown request type:{ %d }\n", (int)describe->rtype);
+  }
+  removeDescribe(res->rid());
+}
 
-            using RequestCallback = std::function<void(const BaseMessage::ptr &)>;
-            using AsyncResponse = std::future<BaseMessage::ptr>;
-            struct RequestDescribe
-            {
-                using ptr = std::shared_ptr<RequestDescribe>;
-                RType rtype;
-                BaseMessage::ptr request;                // 请求消息
-                std::promise<BaseMessage::ptr> response; // 通过get_future()可以获取一个与该promise相关联的future对象
-                RequestCallback callback;
-            }; // struct RequestDescribe
+bool Requestor::send(const BaseConnection::ptr &con,
+                     const BaseMessage::ptr &req,
+                     const RequestCallback &callback) {
 
-            void onResponse(const BaseConnection::ptr &, BaseMessage::ptr &); // 注册进Dispatcher中的对响应进行的业务处理 需要调用 Callback
+  auto rdp =
+      newDestribe(req, RType::REQ_CALLBACK, callback); // 创建一个请求描述
+                                                       // 留了一手 做个判断
+  if (!rdp.get()) {
+    ELOG("Requestor Destribe 对象构造失败\n");
+    return false;
+  }
+  con->send(req); // 发送请求
+  return true;
+}
 
-            /* 为用户提供两个send接口 通过send接口向服务端发送数据*/
-            bool send(const BaseConnection::ptr &, const BaseMessage::ptr &, const RequestCallback &); // 回调请求
+bool Requestor::send(const BaseConnection::ptr &con,
+                     const BaseMessage::ptr &req, AsyncResponse &async_resp) {
+  auto rdp = newDestribe(req, RType::REQ_ASYNC); // 异步请求
+  if (rdp.get() ==
+      nullptr) { // 一般情况下没有创建失败的可能
+                 // 因为一般构造失败时对应的 newDescribe
+                 // 函数中的 make_shared 将会提前抛异常终止程序向下运行
+    ELOG("Requestor Destribe 对象构造失败\n");
+    return false;
+  }
+  DLOG("Rqeuestor Destribe 对象构造成功\n");
+  ////////DEBUG↓//////////////
+  // auto tmprdp = std::dynamic_pointer_cast<RpcRequest>(rdp->request);
+  // Json::Value tmpparms = tmprdp->params();
 
-            bool send(const BaseConnection::ptr &, const BaseMessage::ptr &, AsyncResponse &); // 异步请求
+  // DLOG("\nMTypd: %d\nMethod: %s\nParams: [%d,%d]\nRid: %s\n",
+  // tmprdp->mtype(), tmprdp->method().c_str(), tmpparms["num1"].asInt(),
+  // tmpparms["num2"].asInt(), tmprdp->rid().c_str());
+  ////////DEBUG↑//////////////
+  DLOG("Requestor::send 异步send 准备send");
 
-            bool send(const BaseConnection::ptr &, const BaseMessage::ptr &, BaseMessage::ptr &); // 同步请求
+  con->send(req);
+  async_resp = rdp->response.get_future();
+  return true;
+}
 
-        private:
-            RequestDescribe::ptr newDestribe(const BaseMessage::ptr &, RType, const RequestCallback &cb = nullptr); // 创建一个请求描述
+bool Requestor::send(const BaseConnection::ptr &con,
+                     const BaseMessage::ptr &req,
+                     BaseMessage::ptr &rsp) // 同步请求
+{                                           // 这里是一个同步操作
+  AsyncResponse rsp_future; // 用于接收异步send中的future对象
+  DLOG("Requestor 同步send 准备send");
+  // DLOG("req: %s", req->serialize().c_str());
 
-            RequestDescribe::ptr getDescribe(const std::string &); // 通过rid找到对应的请求描述
+  bool ret = send(con, req, rsp_future);
+  if (!ret) {
+    DLOG("Requestor 同步send 失败\n");
+    return false;
+  }
+  con->send(req);
+  rsp = rsp_future.get(); // 可以直接get进行阻塞(同步) 需要将rsp进行返回
+  return true;
+}
 
-            void removeDescribe(const std::string &); // 通过rid删除对应的请求描述
-        private:
-            std::mutex _mutex;                                                        // 保护_request_describe 临界资源
-            std::unordered_map<std::string, RequestDescribe::ptr> _request_describes; // rid 与 请求描述的映射关系
+Requestor::RequestDescribe::ptr
+Requestor::newDestribe(const BaseMessage::ptr &req, RType rtype,
+                       const RequestCallback &cb) {
+  std::unique_lock<std::mutex> lock(_mutex); // RAII
 
-        }; // class Requestor
+  auto describe = std::make_shared<RequestDescribe>();
+  describe->request = req;
+  describe->rtype = rtype;
+  describe->callback = cb;
+  _request_describes[req->rid()] = describe;
+  return describe;
+}
 
-        void Requestor::onResponse(const BaseConnection::ptr &con, BaseMessage::ptr &res)
-        {
-            /*
-             通过rid找到对应的请求描述
-             若未找到则表示该响应不存在对应的请求描述
-             若找到则判断该响应对应的请求描述的rtype
-             若rtype为RType::SYNC 则调用callback
-             若rtype为RType::ASYNC 则调用promise的set_value
-             处理完毕后删除对应的请求描述 以免内存泄漏
-            */
-            auto describe = getDescribe(res->rid());
-            if (describe.get() == nullptr)
-            {
-                ELOG("not found request describe for response rid:{ %s }\n", res->rid().c_str());
-                return; // 未找到对应的请求描述 无需处理
-            }
-            if (describe->rtype == RType::REQ_CALLBACK)
-            {
-                if (describe->callback) // 若callback存在 则调用callback
-                {
-                    describe->callback(res);    // 同步请求 调用callback
-                    removeDescribe(res->rid()); // 删除对应的请求描述
-                }
-                else
-                {
-                    ELOG("callback is empty"); // 若callback为空 则不调用callback
-                }
-            }
-            else if (describe->rtype == RType::REQ_ASYNC)
-            {
-                describe->response.set_value(res); // 异步请求 调用promise的set_value
-                removeDescribe(res->rid());        // 删除对应的请求描述
-            }
-            else
-            {
-                ELOG("unknown request type:{ %d }\n", (int)describe->rtype);
-            }
-            removeDescribe(res->rid());
-        }
+Requestor::RequestDescribe::ptr Requestor::getDescribe(const std::string &rid) {
+  std::unique_lock<std::mutex> lock(_mutex); // RAII
+  auto it = _request_describes.find(rid);
+  if (it == _request_describes.end()) {
+    return Requestor::RequestDescribe::ptr(); // 返回一个空的智能指针
+  }
+  return it->second; // 找到 返回对应的请求描述
+}
 
-        bool Requestor::send(const BaseConnection::ptr &con, const BaseMessage::ptr &req, const RequestCallback &callback)
-        {
-
-            auto rdp = newDestribe(req, RType::REQ_CALLBACK, callback); // 创建一个请求描述
-                                                                        // 留了一手 做个判断
-            if (!rdp.get())
-            {
-                ELOG("Requestor Destribe 对象构造失败\n");
-                return false;
-            }
-            con->send(req); // 发送请求
-            return true;
-        }
-
-        bool Requestor::send(const BaseConnection::ptr &con, const BaseMessage::ptr &req, AsyncResponse &async_resp)
-        {
-            auto rdp = newDestribe(req, RType::REQ_ASYNC); // 异步请求
-            if (rdp.get() == nullptr)
-            { // 一般情况下没有创建失败的可能
-              // 因为一般构造失败时对应的 newDescribe
-              // 函数中的 make_shared 将会提前抛异常终止程序向下运行
-                ELOG("Requestor Destribe 对象构造失败\n");
-                return false;
-            }
-            DLOG("Rqeuestor Destribe 对象构造成功\n");
-            ////////DEBUG↓//////////////
-            // auto tmprdp = std::dynamic_pointer_cast<RpcRequest>(rdp->request);
-            // Json::Value tmpparms = tmprdp->params();
-
-            // DLOG("\nMTypd: %d\nMethod: %s\nParams: [%d,%d]\nRid: %s\n", tmprdp->mtype(), tmprdp->method().c_str(), tmpparms["num1"].asInt(), tmpparms["num2"].asInt(), tmprdp->rid().c_str());
-            ////////DEBUG↑//////////////
-            DLOG("Requestor::send 异步send 准备send");
-
-            con->send(req);
-            async_resp = rdp->response.get_future();
-            return true;
-        }
-
-        bool Requestor::send(const BaseConnection::ptr &con, const BaseMessage::ptr &req, BaseMessage::ptr &rsp) // 同步请求
-        {                                                                                                        // 这里是一个同步操作
-            AsyncResponse rsp_future;                                                                            // 用于接收异步send中的future对象
-            DLOG("Requestor 同步send 准备send");
-            // DLOG("req: %s", req->serialize().c_str());
-
-            bool ret = send(con, req, rsp_future);
-            if (!ret)
-            {
-                DLOG("Requestor 同步send 失败\n");
-                return false;
-            }
-            con->send(req);
-            rsp = rsp_future.get(); // 可以直接get进行阻塞(同步) 需要将rsp进行返回
-            return true;
-        }
-
-        Requestor::RequestDescribe::ptr Requestor::newDestribe(const BaseMessage::ptr &req, RType rtype, const RequestCallback &cb)
-        {
-            std::unique_lock<std::mutex> lock(_mutex); // RAII
-
-            auto describe = std::make_shared<RequestDescribe>();
-            describe->request = req;
-            describe->rtype = rtype;
-            describe->callback = cb;
-            _request_describes[req->rid()] = describe;
-            return describe;
-        }
-
-        Requestor::RequestDescribe::ptr Requestor::getDescribe(const std::string &rid)
-        {
-            std::unique_lock<std::mutex> lock(_mutex); // RAII
-            auto it = _request_describes.find(rid);
-            if (it == _request_describes.end())
-            {
-                return Requestor::RequestDescribe::ptr(); // 返回一个空的智能指针
-            }
-            return it->second; // 找到 返回对应的请求描述
-        }
-
-        void Requestor::removeDescribe(const std::string &rid)
-        {
-            std::unique_lock<std::mutex> lock(_mutex); // RAII
-            _request_describes.erase(rid);             // 删除对应的请求描述
-        }
-    } // namespace client
+void Requestor::removeDescribe(const std::string &rid) {
+  std::unique_lock<std::mutex> lock(_mutex); // RAII
+  _request_describes.erase(rid);             // 删除对应的请求描述
+}
+} // namespace Client
 
 } // namespace Rpc
--- source/server/rpc_router.hpp
+++ source/server/rpc_router.hpp
@@ -6,19 +6,54 @@
 
 #include "../common/fields.hpp"
 
+/*
+ * 在RpcRouter模块中, 其中RpcRouter模块是暴露给用户所使用,
+ * 主要暴露两个接口, 分别为register注册接口与onRpcRequest接口
+ * onRpcRequest接口用于注册进Dispatcher模块,
+ * Register接口用于给用户进行具体方法的注册
+ * 当一条请求被Dispatcher分发给RpcRouter路由模块后,
+ * 准确是调用RpcRouter模块中的onRpcRequest函数被调用
+ * 在onRpcRequest函数中,
+ * 会检查ServiceManage中的哈希表中是否存在对应方法名的方法, 若是有则进行调用,
+ * 若是没有则直接返回一个Response表示调用出错
+ *
+ * 和具体方法有关的服务管理ServiceManage中存在一个哈希表用于存储方法名称与具体方法描述的映射(pair)
+ * 同时提供了增删查的功能
+ *
+ * 定义了一个新的类为ServiceDescribe作为具体方法的描述,
+ * 具体方法描述中包含服务的名称, 服务的具体调用函数, 服务的参数,
+ * 以及服务的返回值与各种检查, 枚举类Vtype也用于对各字段的检查,
+ * ServiceDescribeFactory是一个建造者模式用于实例化出一个ServiceDescribe实例对象
+ *
+ */
+
 namespace Rpc {
 namespace Server {
 /* -------------------- enum class VType --------------------- */
-enum class VType { BOOL = 0, INTEGRAL, NUMERIC, STRING, ARRAY, OBJECT };
+enum class VType {
+  BOOL = 0, // 布尔类型
+  INTEGRAL, // 整形
+  NUMERIC,  // 浮点数类型
+  STRING,   // 字符串类型
+  ARRAY,    // 数组类型
+  OBJECT    // Json 对象类型
+};
 
 /* -------------------- class ServiceDescribe --------------------- */
 
 class ServiceDescribe // 服务描述
 {
+  /*
+   * ServiceDescribe 模块主要是用于描述一个具体方法的, 如Add等
+   * 并提供相应的检查机制, 即对参数的校验
+   */
 public:
   using ptr = std::shared_ptr<ServiceDescribe>;
   using ServiceCallback = std::function<void(
-      const Json::Value &, Json::Value &)>; // 实际业务处理函数
+      const Json::Value &,
+      Json::Value &)>; // 实际业务处理函数, 两个Json对象其中一个是参数,
+                       // 一个是返回值, 输入型(第一个)参数为参数,
+                       // 输出型参数(第二个)为返回值
   using ParamsDescribe =
       std::pair<std::string /* 参数字段名称 */, VType /* 参数字段类型 */>;
 
@@ -35,13 +70,18 @@ private:
   bool resultCheck(const Json::Value &); // 进行返回值类型校验
 
 private:
-  std::string _method_name;            // 方法名称
-  ServiceCallback _callback;           // 实际业务回调
-  std::vector<ParamsDescribe> _params; // 参数描述
-  VType _result;                       // 返回值类型
+  std::string _method_name;  // 方法名称
+  ServiceCallback _callback; // 实际业务回调
+  std::vector<ParamsDescribe>
+      _params; // 一个服务可能需要多个参数,
+               // 因此采用一个vector容器来存储对应的参数,
+               // 其中ParamsDescribe是一个pair, 用来映射参数与其对应类型的字段
+  VType _result; // 返回值类型描述
 }; // class ServiceDerscibe
 
 bool ServiceDescribe::check(const Json::Value &val, VType vtype) {
+  // 传入一个Vtype用于校验, 随后根据switch-case来判断vtype的具体类型,
+  // 判断val参数类型是否与预置类型一致,
   switch (vtype) {
   case VType::BOOL:
     return val.isBool();
@@ -60,12 +100,23 @@ bool ServiceDescribe::check(const Json::Value &val, VType vtype) {
 }
 
 bool ServiceDescribe::paramsCheck(const Json::Value &params) {
-  for (auto &param_desc : _params) {
-    if (!params.isMember(param_desc.first)) {
+  // 主要用于判断所描述参数字段是否存在, 且类型是否一致
+  for (auto &param_desc :
+       _params) { // 通过范围for去遍历该服务中的参数描述vector<pair<>>
+                  // _params来检查字段是否存在
+    if (!params.isMember(param_desc.first)) { // 检查字段是否存在, pair<>::first
+                                              // 中存储的是一个string类型
       ELOG("%s 参数字段缺失\n", param_desc.first.c_str());
       return false;
     }
-    if (!check(params[param_desc.first], param_desc.second)) {
+    if (!check(
+            params[param_desc.first],
+            param_desc
+                .second)) { // check函数需要传入两个参数,
+                            // 分别为参数名称对应的参数值与参数类型ValueType
+                            // 通过取出对应的值,
+                            // 并在check函数中进行构造成Value对象,
+                            // 直接对val进行isXXXX的类型判断来判断类型是否正确
       ELOG("%s 参数字段类型不匹配\n", param_desc.first.c_str());
       return false;
     }
@@ -78,6 +129,7 @@ bool ServiceDescribe::resultCheck(const Json::Value &result) {
 }
 
 bool ServiceDescribe::call(const Json::Value &params, Json::Value &result) {
+  // 调用业务处理函数, result为输出型参数, 结果返回后需要对结果的类型进行判断
   _callback(params, result);
   if (!resultCheck(result)) {
     ELOG("响应信息校验失败\n");
@@ -92,6 +144,10 @@ const std::string &ServiceDescribe::method() { return _method_name; }
 
 class ServiceDescribeFactory // 建造者模式
 {
+  /*
+   * 该类主要通过set函数将参数设置进ServiceDescribeFactory建造者中
+   * 再通过该建造者模式中的build去构造一个ServiceDescribe实例
+   */
 public:
   // set 设置参数.......
   void setCallBack(const ServiceDescribe::ServiceCallback &);
@@ -136,7 +192,8 @@ public:
   void remove(const std::string &);                 // 删除一个服务
 private:
   std::mutex _mutex; // 保护临界资源_services
-  std::unordered_map<std::string, ServiceDescribe::ptr> _services;
+  std::unordered_map<std::string, ServiceDescribe::ptr>
+      _services; // 用于存储具体服务名称与服务描述的映射的哈希表
 }; // class ServiceManager
 
 void ServiceManager::insert(const ServiceDescribe::ptr &desc) {
@@ -144,14 +201,16 @@ void ServiceManager::insert(const ServiceDescribe::ptr &desc) {
   _services.insert({desc->method(), desc});
 }
 ServiceDescribe::ptr ServiceManager::select(const std::string &mname) {
-  std::unique_lock<std::mutex> lock(_mutex);
+  std::unique_lock<std::mutex> lock(
+      _mutex); // 选择对应的服务的服务描述并返回对应的服务描述
   auto desc = _services.find(mname);
   if (desc == _services.end()) {
     return ServiceDescribe::ptr();
   }
   return desc->second;
 }
-void ServiceManager::remove(const std::string &mname) {
+void ServiceManager::remove(
+    const std::string &mname) { // 删除哈希表中的对应的某个服务
 
   std::unique_lock<std::mutex> lock(_mutex);
   _services.erase(mname);
@@ -176,6 +235,9 @@ private:
   ServiceManager::ptr manager_; // 服务管理器
 }; // class RpcRouter
 
+// Rpc::Router::onRpcRequest 函数是注册进Dispatcher中的函数,
+// 当消息到来时将会检查消息的MType,
+// 当消息为RpcRequest时在哈希表中寻找映射并调用onRpcRequest函数对Rpc请求进行处理
 void RpcRouter::onRpcRequest(const BaseConnection::ptr &con,
                              RpcRequest::ptr &req) {
   /*
@@ -185,14 +247,18 @@ void RpcRouter::onRpcRequest(const BaseConnection::ptr &con,
       4. 处理完毕后组织响应 返回结果
   */
   // 判断是否能够提供服务
-  auto service = manager_->select(req->method());
+  auto service = manager_->select(
+      req->method()); // 传入一个method方法名称在哈希表中寻找是否存在可提供的方法
+
   if (!service.get()) {
     // 失败 不存在可提供的服务 需要构建响应并返回
     ELOG("未找到对应服务: %s\n", req->method().c_str());
     return response(con, req, Json::Value(), RCode::RCODE_NOT_FOUND_SERVICE);
   }
   // 进行参数校验
-  if (!service->paramsCheck(req->params())) {
+  if (!service->paramsCheck(
+          req->params())) { // params() 是一个JsonObject对象类型
+
     ELOG("参数校验失败: %s\n", req->method().c_str());
     return response(con, req, Json::Value(), RCode::RCODE_INVALID_PARAMS);
   }
